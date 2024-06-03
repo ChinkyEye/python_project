41d75cda96df11f7f95482d846bc97a48f5b1c42
Your Name <you@example.com>
2024-06-03 12:57:48 +0545

    conditional statement

--- try.py
+++ try.py
@@ -1,3 +1,11 @@
 a = eval(input("Enter the number = "))
 b =eval(input(" Enter the number = "))
 print(a + b)
+
+
+if a%2==0:
+    print("Even number")
+elif a%2 == 1.52:
+    print("Odd number")
+else:
+    print("error")
