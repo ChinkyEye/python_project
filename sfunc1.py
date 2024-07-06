207f9d4f99b9951e175ef421c6d0fe9f4cbb7671
Your Name <you@example.com>
2024-07-06 20:52:19 +0545

    git chr and ord

---
+++ sfunc1.py
@@ -0,0 +1,9 @@
+w = "WeWlcome to Kerabari"
+w1 = "123"
+# print(w.find('W',1))
+# print(w.find('w'))
+# print(w.index('e'))
+
+print(w.isalpha())
+print(w1.isdigit())
+print(w.isalnum())
---
+++ sfunc2.py
@@ -0,0 +1,4 @@
+y = chr(65)
+z = ord('B')
+print(type(y),y)
+print(type(z),z)
