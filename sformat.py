ba09f2f7d2b8406906cf13dcc3a28b82efcd4c9f
Your Name <you@example.com>
2024-07-07 14:20:53 +0545

    python format

---
+++ sformat.py
@@ -0,0 +1,6 @@
+text = "Welcome to {name:>10} {surname}".format(name = "Milan", surname = "khimding")
+text1 = "Namaste {}".format("Nepal")
+text2 = "Namaste {1}".format("Nepal","Milan")
+print(text)
+print(text1)
+print(text2)
