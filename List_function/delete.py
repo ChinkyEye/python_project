cf8897f9767b21fbcd62a08e9a333ae262369fcf
Your Name <you@example.com>
2024-07-13 16:44:23 +0545

    list function

---
+++ List_function/delete.py
@@ -0,0 +1,8 @@
+l1 = [1, 2, 3, 4, 5]
+
+# del l1[0]
+
+# print(l1.pop(2))
+# l1.remove(5)
+l1.clear()
+print(l1)
---
+++ List_function/update.py
@@ -0,0 +1,8 @@
+l1 = [10, 11, 12, 13]
+l1[0] = 20
+l1.insert(0, 40)
+l2 = ["khimding", "limbu"]
+l1.append("milan")
+l1.append(l2)
+l1.extend(l2)
+print(l1)
