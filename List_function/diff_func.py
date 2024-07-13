7ca066b05b0fc28986b5fd5c2a7e37cc1760c6a5
Your Name <you@example.com>
2024-07-13 17:05:47 +0545

    list diff function

---
+++ List_function/diff_func.py
@@ -0,0 +1,20 @@
+l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+l2 = ["milan", "khimding"]
+total_number = l1.count(10)
+min_number = min(l1)
+max_number = max(l2)
+print(total_number, max_number, min_number)
+
+# sorting in list
+l3 = [20, 10, 15]
+l3.sort()
+print(l3)
+
+# reversing in list
+
+l3.reverse()
+print(l3)
+
+# indexing in list
+print(l3.index(20))
+
